251c20108a6458f2efae49012c46cd2d62fb818e
Phil Goodwin <phil.goodwin@gmail.com>
2020-11-22 14:50:19 -0800

    Start playing with tags and named threads

--- app/main.rb
+++ app/main.rb
@@ -3,21 +3,25 @@
 require "semantic_logger"
 
 SemanticLogger.default_level = :info
-SemanticLogger.add_appender(io: $stdout, level: :info, formatter: :color)
+SemanticLogger.add_appender(io: $stdout, formatter: :color)
 
 class LoggableClass
   include SemanticLogger::Loggable
 
   def self.send_log(message)
-    logger.tagged(method_type: :class)
-    logger.info message
+    logger.tagged(method_type: :class) do
+      logger.info message
+    end
   end
 
   def send_log(message)
-    logger.tagged(method_type: :instance)
-    logger.info message
+    logger.tagged(method_type: :instance) do
+      logger.info message
+    end
   end
 end
+
+Thread.current.name = "main thread"
 
 LoggableClass.send_log :hi
 LoggableClass.new.send_log :mom
